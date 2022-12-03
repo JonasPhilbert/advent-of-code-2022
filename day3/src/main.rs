188c9c59d6a38478c2653454692469c71818a22c
Jonas Philbert <6285509+JonasPhilbert@users.noreply.github.com>
2022-12-03 23:36:48 +0100

    Part two :tada:

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,11 +4,16 @@ fn main() {
     assert_eq!(score(&'A'), 27);
     assert_eq!(score(&'Z'), 52);
 
+    part_one();
+    part_two();
+}
+
+fn part_one() {
     let mut sum = 0;
     let input = std::fs::read_to_string("input").unwrap();
-    let parsed: Vec<(&str, &str)> = input.split("\n").map(|row| row.split_at(row.len() / 2)).collect();
+    let rucksacks: Vec<(&str, &str)> = input.split("\n").map(|row| row.split_at(row.len() / 2)).collect();
 
-    for rucksack in parsed {
+    for rucksack in rucksacks {
         assert_eq!(rucksack.0.len(), rucksack.1.len());
         for item_a in rucksack.0.chars() {
             let mut found = false;
@@ -23,7 +28,27 @@ fn main() {
         }
     }
 
-    println!("{}", sum);
+    println!("#1: {}", sum);
+}
+
+fn part_two() {
+    let mut sum = 0;
+
+    let input = std::fs::read_to_string("input").unwrap();
+    let rucksacks: Vec<&str> = input.split("\n").collect();
+    for i in (0..rucksacks.len() - 3).step_by(3) {
+        let a = rucksacks[i];
+        let b = rucksacks[i + 1];
+        let c = rucksacks[i + 2];
+        for char in a.chars() {
+            if b.contains(char) && c.contains(char) {
+                sum += score(&char);
+                break;
+            }
+        }
+    }
+
+    println!("#2: {}", sum);
 }
 
 fn score(char: &char) -> u32 {
